993cd24262c069085a4f5f41537d92a824d2ccdc
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-12-08 16:04:54 +0100

    ADD: fetch data trough axios from express api

--- src/pages/PostsList/PostsList.jsx
+++ src/pages/PostsList/PostsList.jsx
@@ -1,9 +1,31 @@
 import style from "./PostsList.module.css"
+import { useEffect, useState } from "react"
+import axios from "axios"
 import sectionsStyle from "../Sections.module.css"
 import posts from "../../data/posts.js"
 import Card from "../../components/Card/Card.jsx"
 
+export const API_BASE_URI = "http://localhost:3000/"
+
 function PostsList() {
+
+    const [posts, setPosts] = useState([])
+
+    function fetchPosts() {
+        axios.get(`${API_BASE_URI}posts`)
+            .then((res) => {
+                setPosts(res.data)
+            })
+            .catch((err) => {
+                console.error(err)
+            })
+    }
+
+    useEffect(() => {
+        fetchPosts()
+    }, [])
+
+
     return (
         <>
             <main>
